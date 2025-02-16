fef3ff497080767fb8dd36ac2dc9f9ad7727f795
ilonaGold <songa17@inbox.lv>
2025-02-16 16:33:05 +0200

    feat: implement deleteDigit function

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,9 +11,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function deleteDigit(n) {
+  if (typeof n !== 'number' || isNaN(n)) {
+    throw new Error('Input must be a valid number');
+  }
+
+  let nStr = n.toString();
+  let max = 0;
+
+  for (let i = 0; i < nStr.length; i++) {
+    let num = nStr.slice(0, i) + nStr.slice(i + 1);
+    if (num > max) {
+      max = num;
+    }
+  }
+
+  return +max;
 }
 
 module.exports = {
